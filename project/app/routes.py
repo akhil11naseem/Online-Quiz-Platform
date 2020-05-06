cafdfd5896a30e7fef4420a502a51fa46d92b0f1
jaivar15 <varun.jain.au@gmail.com>
2020-05-06 14:49:59 +0800

    Login System with default username/password, with student manage and class pages added

--- project/app/routes.py
+++ project/app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template 
+from flask import render_template, flash, url_for, redirect
 from app import app
 from app.forms import LoginForm, RegistrationForm
 
@@ -9,9 +9,23 @@ def index():
 @app.route('/login', methods=['GET','POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'admin@admin.com' and form.password.data == 'password':
+            flash('You have been successfully logged in', 'success')
+            return(redirect(url_for('manage_students')))
+        else: 
+            flash('Login Failed. Please check password and username', 'error')
     return render_template('login.html', title = "Login Form", form = form)
 
 @app.route('/register')
 def register():
     form = RegistrationForm()
     return render_template('register.html', title = "Register", form = form)
+
+@app.route('/students')
+def manage_students():
+    return render_template('manage-students.html', title="Students-Admin")
+
+@app.route('/scores')
+def scores():
+    return render_template('class-scores.html', title="Students-Admin")
--- project/venv/bin/activate.fish
+++ project/venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/Shalini/Desktop/project/venv"
+set -gx VIRTUAL_ENV "/Users/Shalini/Desktop/CITS5505-Online-Quiz-Platform/project/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
