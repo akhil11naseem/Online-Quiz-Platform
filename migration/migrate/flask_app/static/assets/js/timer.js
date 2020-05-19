e76a4b7c10355902f067c80db0732be03e9ef77c
jaivar15 <varun.jain.au@gmail.com>
2020-05-19 15:18:32 +0800

    migration include with some comments

---
+++ migration/migrate/flask_app/static/assets/js/timer.js
@@ -0,0 +1,37 @@
+function getTimeRemaining(endtime) {
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor((t / 1000) % 60);
+  var minutes = Math.floor((t / 1000 / 60) % 60);
+  var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
+  var days = Math.floor(t / (1000 * 60 * 60 * 24));
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+function initializeClock(id, endtime) {
+  
+  function updateClock() {
+    var t = getTimeRemaining(endtime);
+
+    document.getElementById(id).innerHTML = ('0' + t.minutes).slice(-2) + ":" + ('0' + t.seconds).slice(-2);
+
+    if (t.total <= 0) {
+      clearInterval(timeinterval);
+    }
+  }
+
+  updateClock();
+  var timeinterval = setInterval(updateClock, 1000);
+}
+
+
+var timeInMinutes = 3;
+var currentTime = Date.parse(new Date());
+var deadline = new Date(currentTime + timeInMinutes*60*1000);
+
+initializeClock('timer-box', deadline);
