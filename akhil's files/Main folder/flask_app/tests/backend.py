a2d2c24e65f462d56e5a5da71a7450c7bf15a270
jaivar15 <varun.jain.au@gmail.com>
2020-05-21 19:26:41 +0800

    early testing stages

--- akhil's files/Main folder/flask_app/tests/backend.py
+++ akhil's files/Main folder/flask_app/tests/backend.py
@@ -1,41 +1,63 @@
 import unittest, os
-
 from ..extensions import db
 from ..commands import create_tables
 from ..models import User, Topic, Results
 from flask_testing import TestCase
 from .. import create_app
+from flask import url_for
 
 app=create_app(config_file='test_settings.py')
-
+app.testing = True
 app.test_client()
-
 
 class UserModelTest(unittest.TestCase):
     # will be called before every test
 
+
+
     def setUp(self):
         """Defines what should be done before every single test in this test group."""
 
-        # create test admin user
-        admin = User(username="admin", password="admin", admin=True, student=False, enabled=True)
-
-        # create test non-admin user
-        student = User(username="student1", password="student1", admin=False, student=True, enabled=True)
-
         with app.app_context():
             db.create_all()
+            # create test admin user
+            admin = User(username="admin", password="admin", admin=True, student=False, enabled=True)
+            
+            # create test non-admin user
+            student = User(username="student1", password="student1", admin=False, student=True, enabled=True)
             db.session.add(admin)
             db.session.add(student)
             db.session.commit()
-
 
     def tearDown(self):
         """Defines what should be done after every single test in this test group."""
         with app.app_context():
             db.session.remove()
-            db.drop_all()
+    
 
+    
+   
+
+class TestModel(unittest.TestCase):
+    
+    def test_login_system(self):
+        tester = app.test_client(self)
+        response = tester.post (
+            '/login',
+            data = dict(username="admin", password="password"),
+            follow_redirects=True
+        )
+        self.assertIn(b'Invalid credentials, please try again', response.data)
+
+
+    def test_root(self):
+        tester = app.test_client()
+        response = tester.get('/', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
+    
+    def test_password(self):
+        self.assertEqual(3,3)
+    
     def test_password_hashing(self):
         self.assertEqual(1,1)
 
