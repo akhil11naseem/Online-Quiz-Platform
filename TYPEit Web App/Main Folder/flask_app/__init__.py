d40cab592d76cc05108f9a3fa2cdaa07c29bb453
jaivar15 <varun.jain.au@gmail.com>
2020-05-23 18:09:58 +0800

    migrations implemented

--- TYPEit Web App/Main Folder/flask_app/__init__.py
+++ TYPEit Web App/Main Folder/flask_app/__init__.py
@@ -4,15 +4,26 @@ from .commands import create_tables
 from .routes.main import main
 from .routes.auth import auth
 from .models import User
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Manager
+
 
 def create_app(config_file = 'settings.py'):
-
+    #create an app and configure it
     app = Flask(__name__)
 
     app.config.from_pyfile(config_file)
     app.app_context().push()
-    db.init_app(app)
+   
 
+
+    migrate = Migrate(app, db)
+    manager = Manager(app)
+    manager.add_command('db', MigrateCommand)
+    db.init_app(app)
+    
+    
+    #Configure the login_manager object 
     login_manager.init_app(app)
 
     login_manager.login_view = 'auth.login'
